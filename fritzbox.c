4187d92f7f2e041798d055193e8ee5a4e66cd7f4
Joachim Wilke <github.com@joachim-wilke.de>
2009-05-28 18:02:09 +0000

    - changed Plugin Name from "Fritz!Box Plugin..." to "Fritz Plugin ..." and adapted translations accordingly - incremented version to 1.2.0

--- fritzbox.c
+++ fritzbox.c
@@ -28,8 +28,8 @@
 #include "notifyosd.h"
 #include "menu.h"
 
-static const char *VERSION        = "1.1.7";
-static const char *DESCRIPTION    = trNOOP("Fritz!Box Plugin for AVM Fritz!Box");
+static const char *VERSION        = "1.2.0";
+static const char *DESCRIPTION    = trNOOP("Fritz Plugin for AVM Fritz!Box");
 static const char *MAINMENUENTRY  = trNOOP("Fritz!Box");
 
 cPluginFritzbox::cPluginFritzbox(void)
