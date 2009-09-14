3533c38a465ff6c25c89de4c60a4723297f7d4ba
Joachim Wilke <git@joachim-wilke.de>
2009-09-14 19:49:13 +0000

    - minor changes - debug target for eclipse

--- libfritz++/FritzFonbook.cpp
+++ libfritz++/FritzFonbook.cpp
@@ -1,7 +1,7 @@
 /*
  * libfritz++
  *
- * Copyright (C) 2007-2008 Joachim Wilke <vdr@joachim-wilke.de>
+ * Copyright (C) 2007-2009 Joachim Wilke <vdr@joachim-wilke.de>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
